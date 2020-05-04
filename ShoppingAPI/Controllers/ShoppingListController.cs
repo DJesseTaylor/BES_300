840c89b9a212916eaa9aa8a944af0e5a56978ecc
Jesse Taylor <8312896+DJesseTaylor@users.noreply.github.com>
2020-05-04 16:07:03 -0400

    End of Day one

--- ShoppingAPI/Controllers/ShoppingListController.cs
+++ ShoppingAPI/Controllers/ShoppingListController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingAPI.Data;
 using ShoppingAPI.Models;
 using System;
 using System.Collections.Generic;
@@ -9,10 +11,25 @@ namespace ShoppingAPI.Controllers
 {
     public class ShoppingListController : ControllerBase
     {
+        private readonly ShoppingDataContext DataContext;
+
+        public ShoppingListController(ShoppingDataContext dataContext)
+        {
+            DataContext = dataContext;
+        }
+
         [HttpGet("shoppinglist")]
         public async Task<ActionResult> GetFullShoopingList()
         {
             var response = new GetShoppingListResponse();
+            response.Data = await DataContext.ShoppingItems
+                .Select(item => new ShoppingListItemResponse
+                {
+                    Id = item.Id,
+                    Description = item.Description,
+                    Purchased = item.Purchased,
+                    PurchasedFrom = item.PurchasedFrom
+                }).ToListAsync();
             return Ok(response);
         }
     }
--- ShoppingAPI/Program.cs
+++ ShoppingAPI/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ShoppingAPI.Data;
 
 namespace ShoppingAPI
 {
@@ -13,7 +14,7 @@ namespace ShoppingAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().MigrateDatabase<ShoppingDataContext>().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
